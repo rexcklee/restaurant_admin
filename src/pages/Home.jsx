f61875ed0d09f0cb349f69a12dc0507ffbd4a85c
clee5rrc <clee5@academic.rrc.ca>
2024-05-01 12:46:02 -0500

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <>
+      <p>Home page</p>
+    </>
+  );
+}
+
+export default Home;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <>
+      <p>Login page</p>
+    </>
+  );
+}
+
+export default Login;
