45c5bd877dba630bb7e72f42a809d8b3fd88faf2
rexcklee <rexcklee852@gmail.com>
2025-01-10 11:55:09 -0600

    remove api host name

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -12,7 +12,6 @@ class DataResponse {
 
 class API {
   apiHost: string = import.meta.env.VITE_API_URL;
-  // apiHost: string = "https://restaurant-server.rexlee.space";
 
   protected async get(
     path: string,
